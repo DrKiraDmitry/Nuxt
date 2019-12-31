f08c2c5756e4dbd12f4a2999b2400f5fcc7267d6
Dima <stalker2807sky@yandex.ru>
2020-01-01 02:33:49 +0700

    psi

--- Mikiri/pages/index.vue
+++ Mikiri/pages/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="index">
         <div class="synergy row no-gutters">
-            <div class="twoFace col-10 col-xl-4 offset-1">
+            <div class="twoFace col-8 col-xl-4 offset-1">
                 <img src="~/assets/img/twoFaceD.png" alt="">
                 <img src="~/assets/img/twoFace.png" alt="">
             </div>
-            <div class="col-xl-4 col-10 offset-2">
+            <div class="col-xl-4 col-10 offset-xl-2 offset-1">
                 <h3>Синергия</h3>
                 <p>Во мне сочетается много всего, мне очень нравится искусство, точные науки и психология.</p>
                 <p>И здесь я постраюсь отобразить, то что порождает эта Синергия</p>
             </div>
         </div>
         <div class="DeusEx row no-gutters">
-            <animation class="col-5" />
-            <div class="col-4 offset-2">
+            <animation class="col-xl-5 col-12" />
+            <div class="col-xl-4 col-10 offset-xl-2 offset-1">
                 <h3>Начало положенно</h3>
                 <p>Изначально было желание поиграться с векторной анимацией, хотелось разобраться как это делать.</p>
                 <p>В плейлисте заиграл OST Deus Ex: Human Revolution, и мне вспомнилась аниация на начальном экране, как портрет Адама, разбиралася и собирался по кусочкам.</p>
@@ -21,18 +21,26 @@
                 <p>Пришлось заморочится, чтоб каждый элемент, улетал по отдельности</p>
             </div>             
         </div>
-        <div class="index__psychology">
-            <img src="~/assets/img/psi3.png" alt="">
+        <div class="row no-gutters index__psychology">
+            <div class="col-xl-4 offset-1 col-10">
+                <img src="~/assets/img/psi31.png" alt="">
+            </div>
+            <div class="col-xl-4 col-10 offset-xl-2 offset-1">
+                <h3>Начало положенно</h3>
+                <p>Изначально было желание поиграться с векторной анимацией, хотелось разобраться как это делать.</p>
+                <p>В плейлисте заиграл OST Deus Ex: Human Revolution, и мне вспомнилась аниация на начальном экране, как портрет Адама, разбиралася и собирался по кусочкам.</p>
+                <p>Тогда стало ясно, какую аниацию я хочу.</p>
+                <p>Пришлось заморочится, чтоб каждый элемент, улетал по отдельности</p>
+            </div> 
         </div>
     </div>
 </template>
 
 <script>
 import animation from '~/components/animation.vue'
-
 export default {
     components:{
-        animation
+        animation,
     },
     data() {
         return {
